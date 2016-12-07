646186a7388373ffffe243e90a57f7c268709ab3
xuming <507153809@qq.com>
2016-12-07 09:49:03 +0800

    update customDictionary. xuming 20161207

--- src/test/java/org/xm/xmnlp/demo/DemoCustomDictionary.java
+++ src/test/java/org/xm/xmnlp/demo/DemoCustomDictionary.java
@@ -18,14 +18,20 @@ public class DemoCustomDictionary {
     public static void main(String[] args) {
         // 动态增加
         CustomDictionary.add("攻城狮");
+
+        CustomDictionary.add("黄色小电影", "bad 10");
         // 强行插入
         CustomDictionary.insert("白富美", "nz 1024");
         // 删除词语（注释掉试试）
 //        CustomDictionary.remove("攻城狮");
+        CustomDictionary.remove("法轮功");
+
+        CustomDictionary.insert("法轮功", "bad 1000");
         System.out.println(CustomDictionary.add("单身狗", "nz 1024 n 1"));
         System.out.println(CustomDictionary.get("单身狗"));
 
-        String text = "攻城狮逆袭单身狗，迎娶白富美，走上人生巅峰，这是AceBeverage公司的中华人名共和国的新世纪。结婚的和尚未结婚的，";  // 怎么可能噗哈哈！
+        String text = "攻城狮逆袭单身狗，迎娶白富美，走上人生巅峰，这是AceBeverage公司的中华人名共和国的新世纪。" +
+                "结婚的和尚未结婚的，他有一部AV黄色小电影，还是一个法轮功疯子";  // 怎么可能噗哈哈！
 
         // DoubleArrayTrie分词
         final char[] charArray = text.toCharArray();
